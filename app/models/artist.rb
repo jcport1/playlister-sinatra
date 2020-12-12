f76b74627b1352f7418a5f6ffe7a553dde004ebc
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-12-11 21:04:56 -0500

    commit tree f3ec5cdcd9d3588f004ad78be03aa8b0c535cafe

---
+++ app/models/artist.rb
@@ -0,0 +1,8 @@
+class Artist < ActiveRecord::Base
+
+    include Slugifiable::InstanceMethods
+  
+    has_many :songs
+    has_many :genres, through: :songs
+
+  end
---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,10 @@
+module Slugifiable
+
+    module InstanceMethods
+        
+      def slug
+        self.name.downcase.gsub(" ", "-")
+      end
+    end
+  end
+  
